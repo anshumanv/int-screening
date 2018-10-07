302305bad1f42c6373f3754829112360df75f58d
Anshuman <anshu.av97@gmail.com>
2018-10-07 12:11:33 +0530

    Fix auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import * as firebase from 'firebase';
 import { Provider } from 'react-redux';
+import { signedIn, signedOut } from './actions/authActions';
+import { connect } from 'react-redux';
 import NotFound from './containers/NotFound'
 import Admin from './containers/Admin'
 import Interview from './containers/Interview'
@@ -9,32 +11,47 @@ import Start from './containers/Start'
 import { config } from './config'
 import { getStore, getState } from './store/store';
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
-const store = getStore();
-
-export default class App extends Component {
+class App extends Component {
     componentDidMount() {
-
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+              firebase.database().ref('/users/').on('value', snapshot => {
+                if (!snapshot.hasChild(user.uid)) {
+                  firebase.database().ref('/users/' + user.uid).set({
+                    email: user.email,
+                    name: user.displayName,
+                    uid: user.uid
+                  })
+                }
+              });
+              this.props.signedIn(user);
+            } else {
+              this.props.signedOut();
+            }
+      });
     }
     render() {
         return(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Switch>
-                    <Route
-                        exact
-                        path="/"
-                        render={() => {
-                        return <Start />;
-                        }}
-                    />
-                    <Route exact path="/interview/:interviewID" component={Interview}/>
-                    <Route exact path="/admin" component={Admin} />
-                    <Route exact path="*" component={NotFound } />
-                    </Switch>
-                </BrowserRouter>
-            </Provider>
+            <Start />
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+      signedIN: state.auth.signedIn,
+    };
+};
+
+
+export default
+    connect(
+      mapStateToProps,
+      {
+        signedIn,
+        signedOut,
+      },
+    )(App);
+
