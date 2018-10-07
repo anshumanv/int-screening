2b4e322dedc31a84c523be703098d3805f4598ab
Anshuman <anshu.av97@gmail.com>
2018-10-07 11:30:14 +0530

    Firebase auth setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import * as firebase from 'firebase';
+import { Provider } from 'react-redux';
 import NotFound from './containers/NotFound'
 import Admin from './containers/Admin'
 import Interview from './containers/Interview'
 import Start from './containers/Start'
+import { config } from './config'
+import { getStore, getState } from './store/store';
+
+firebase.initializeApp(config);
+
+const store = getStore();
 
 export default class App extends Component {
+    componentDidMount() {
+
+    }
     render() {
         return(
-            <BrowserRouter>
-                <Switch>
-                <Route
-                    exact
-                    path="/"
-                    render={() => {
-                    return <Start />;
-                    }}
-                />
-                <Route exact path="/interview/:interviewID" component={Interview}/>
-                <Route exact path="/admin" component={Admin} />
-                <Route exact path="*" component={NotFound } />
-                </Switch>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => {
+                        return <Start />;
+                        }}
+                    />
+                    <Route exact path="/interview/:interviewID" component={Interview}/>
+                    <Route exact path="/admin" component={Admin} />
+                    <Route exact path="*" component={NotFound } />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
         )
     }
 }
