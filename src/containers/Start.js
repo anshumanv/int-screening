22d27b1551981d83ef8ef3e0df8446e5170f546a
Anshuman <anshu.av97@gmail.com>
2018-10-07 19:05:29 +0530

    auth or redirect

--- src/containers/Start.js
+++ src/containers/Start.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import Auth from '../components/authButton';
 import { connect } from 'react-redux';
 import Quiz from '../components/Quiz'
+import { Button } from 'antd';
+import {Link} from 'react-router-dom'
 
 class Start extends Component {
+    
     render() {
         const signedIn = this.props.signedIn;
         return(
             <div>
-                {this.props.signedIn && <Auth />}
+                {!this.props.signedIn ? <Auth />: 
+                    <Link to={'/interview/Web'}><Button>
+                    Take Test</Button>
+                    </Link>
+                    }
             </div>
         )
     }
